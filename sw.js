3d19fc011545e3cc495efccfc1a34dc46c348509
Silvana Macedo <79228036+sgmacedo-dev@users.noreply.github.com>
2026-09-24 15:49:26 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE = "mesa-cirio-v2";
+const PRECACHE = [
+  "./index.html",
+  "./manifest.webmanifest",
+  "./img/icon-192.png",
+  "./img/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.protocol === "file:") return;
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(event.request, copy));
+        }
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
